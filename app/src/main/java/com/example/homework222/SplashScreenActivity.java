1ceab0179c3b0439eebe3b0031e4b6fb7205a4a4
NatalyVolegova <info@volegova.ru>
2019-07-10 22:57:43 +0300

    changes splash screen

--- app/src/main/java/com/example/homework222/SplashScreenActivity.java
+++ app/src/main/java/com/example/homework222/SplashScreenActivity.java
@@ -2,15 +2,29 @@ package com.example.homework222;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
